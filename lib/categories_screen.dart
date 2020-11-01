c2da719c989223cd6a9d56478fe2c5a92bb336a3
satyendrasingh8 <satyendrasinghsss7398@gmail.com>
2020-11-01 10:01:21 +0530

    add category navigation page

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -10,7 +10,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('DeliMeal'),
       ),
       body: GridView(
-        padding:const EdgeInsets.all(25),
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES.map((catData) {
           return CategoryItem(catData.title, catData.color);
         }).toList(),
--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,17 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
        
-        primarySwatch: Colors.blue,
-       
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          bodyText2:  TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          headline6:TextStyle(fontSize: 20,
+          fontFamily: 'Roboto',
+          fontWeight: FontWeight.bold) 
+        ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: CategoriesScreen(),
