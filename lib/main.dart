6617a0eb748b22e5105020cd3c65be093e38a107
satyendrasingh8 <satyendrasinghsss7398@gmail.com>
2020-11-02 07:55:07 +0530

    filling recipe meal detaill page

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,14 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (ctx) => CategoriesScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
-        MealDetailScreen.routeName:(ctx) => MealDetailScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+      },
+      onGenerateRoute: (settings) {
+        print(settings.arguments);
+        print(settings.name);
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
       },
     );
   }
